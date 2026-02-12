1bd396895075d00ac25bd72a57120a47eb5bada9
kundalik5545 <kundalikjadhav0807@gmail.com>
2026-02-12 20:23:28 +0530

    Refactor for loops example by removing comments and improving docstring clarity

--- loops/for_loops/01_for_loops.py
+++ loops/for_loops/01_for_loops.py
@@ -1,11 +1,18 @@
-# lists = ["apple","banana","orange","mango"]
+ 
+''' 
+For Loops in Python
 
-# for l in lists:
-#     print(f"My fav fruits is {l}!")
+  - For Loops are used to iterate over a sequence (like a list, tuple, string) or other iterable objects. They allow you to execute a block of code     repeatedly for each item in the sequence.
+'''
 
-# name = 'Abhishek'
-# for i in name:
-#     print(i, end="! ")
+lists = ["apple","banana","orange","mango"]
+
+for l in lists:
+    print(f"My fav fruits is {l}!")
+
+name = 'Abhishek'
+for i in name:
+    print(i, end="! ")
 
 
 
