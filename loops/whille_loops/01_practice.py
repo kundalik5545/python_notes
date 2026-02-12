6a348095296419937b6e64f117cc2f4d15f71477
kundalik5545 <kundalikjadhav0807@gmail.com>
2026-02-12 20:26:40 +0530

    Add docstring and comments for while loops practice example

--- loops/whille_loops/01_practice.py
+++ loops/whille_loops/01_practice.py
@@ -1,3 +1,11 @@
+
+'''
+Docstring for loops.whille_loops.01_practice
+Author: Kundalik Jadhav
+
+While Loops Practice
+'''
+
 my_num = int(input("Enter any number: "))
 
 i = 1
@@ -6,3 +14,12 @@ while(i < 11):
     print(f"{i} X {my_num} = {i * my_num}")
     i+=1
 
+
+'''
+inital condition: i = 1
+
+while(logical condition):
+    printing statement
+    
+    increment/decrement statement
+'''
